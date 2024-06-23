37620dc4eedab294c3371030773bc08907ef369f
Val Melamed <valo@vtmelamed.com>
2024-06-23 04:32:08 -0400

    Del TestTypes from json

--- test/ExpressionSerialization/JsonTests/usings.cs
+++ test/ExpressionSerialization/JsonTests/usings.cs
@@ -1,9 +1,4 @@
-﻿global using System.Collections;
-global using System.Collections.Concurrent;
-global using System.Collections.Frozen;
-global using System.Collections.Immutable;
-global using System.Collections.ObjectModel;
-global using System.IO;
+﻿global using System.IO;
 global using System.Linq.Expressions;
 global using System.Runtime.Serialization;
 global using System.Text;
